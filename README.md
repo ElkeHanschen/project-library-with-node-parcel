b23fab49bdcba143ca39f99ce8f68a4cc45d7211
Elke Hanschen <elke.hanschen@oetkerdigital.com>
2020-12-11 16:10:36 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,54 +1,34 @@
-to change!
-
-# Project include external library
+# Project integrate library via node/npm/parcel.js
 
 ### What is this about?
 
-- for desktop (not responsive), build a simple website with HTML, CSS, JavaScript, that [includes an animation of an external library](https://popmotion.io/#quick-start-animation-animate-spring-options)
+- for desktop (not responsive), build a simple website with HTML, CSS, JavaScript, that includes a Spring animation off of the external library [popmotion](https://popmotion.io/)
 - why JavaScript based animation? Quote: "the popmotion animations go beyond the capabilities of CSS (?), so use JavaScript"
-- animation comes in via script tag `<script src=“https://unpkg.com/popmotion@8.1.24/dist/popmotion.global.min.js”></script>` (via CDN) - this is on purpose for rapid experimentation
+- use node.js, npm, parcel.js
+- [take and adapt code from previous tutorial](https://github.com/ElkeHanschen/project-integrate-library)
 
 ### This Repo is based on:
 
 - tutorial within [Frontendmasters' Beginners Path](https://frontendmasters.com/learn/beginner/)
-- tutorial video [Integrating with other people's libraries](https://frontendmasters.com/courses/web-development-v2/integrating-with-other-people-s-libraries/)
-- tutorial course notes [Integrating with other people's libraries](https://btholt.github.io/intro-to-web-dev-v2/libraries)
+- tutorial video [Building your code with node.js](https://frontendmasters.com/courses/web-development-v2/building-your-code-with-node-js/)
+- tutorial course notes [Libraries](https://btholt.github.io/intro-to-web-dev-v2/libraries)
 
 ### Heads-up
 
-- I quickly coded the desired spring animation along during the course, please see, why I mention this here below the code
+- to quickly move on, I needed to pick another animation as the spring animation as shown in the tutorial is not working any longer
 
 ```
-const { styler, spring, listen, pointer, value } = window.popmotion;
-
-const ball = document.querySelector('.anim-box');
-const divStyler = styler(ball);
-const ballXY = value({ x: 0, y: 0 }, divStyler.set);
-
-listen(ball, 'mousedown touchstart')
-    .start((e) => {
-        e.preventDefault();
-        pointer(ballXY.get()).start(ballXY);
-    });
-
-listen(document, 'mouseup touchend')
-    .start(() => {
-        spring({
-            from: ballXY.get(),
-            velocity: ballXY.getVelocity(),
-            to: { x: 0, y: 0 },
-            stiffness: 200,
-            // mass: 1,
-            // damping: 10
-        }).start(ballXY);
-    });
+... add code if necessary ...
 ```
 
-- apparently the popmotion website has been changed since the recording of the tutorial, so there are no codepens linked in it any longer, it’s also not THAT intuitive how to quickly (like, within 2 minutes) grab JavaScript animation code off of this website
-- a quick search resulted in [popmotion on codepen](https://codepen.io/popmotion) but I did not take the time to search if the (up to date) spring animation is in there
+- [popmotion on codepen](https://codepen.io/popmotion)
+- [popmotion code off of codepen I picked](https://codepen.io/popmotion/pen/vZwNap)
 
 ### How to run this?
 
 - clone this repo
 - `cd` into project
-- open `index.html` in your browser of choice (in my case, built in/for Chrome)
+- `npm install`
+- `parcel index.html`
+
+### At the moment, this project is not properly running and needs to be reviewed!
--- index.js
+++ index.js
@@ -1,30 +1,148 @@
-// const styler = window.popmotion.styler;
-// const spring = window.popmotion.spring;
-// const listen = window.popmotion.listen;
-// const pointer = window.popmotion.pointer;
-// const value = window.popmotion.value;
-// destructure this to:
-const { styler, spring, listen, pointer, value } = window.popmotion;
+// https://codepen.io/popmotion/pen/vZwNap
+import popmotion from 'popmotion';
+// const popmotion = require("popmotion");
+// import './useless';
 
+const { pointer, trackOffset, physics, transform, value, css } = popmotion;
+const { clamp, pipe, interpolate, nonlinearSpring, percent, conditional } = transform;
+const SPRING_STRENGTH = 1;
 
-const ball = document.querySelector('.anim-box');
-const divStyler = styler(ball);
-const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+const hue = value(0).setProps({ transform: Math.round });
+const saturation = value(100).setProps({ transform: percent });
+const lightness = value(50).setProps({ transform: percent });
 
-listen(ball, 'mousedown touchstart')
-    .start((e) => {
-        e.preventDefault();
-        pointer(ballXY.get()).start(ballXY);
+class Slider {
+  constructor({ dom, val, getColor, getGradient, initialValue, range }) {
+    this.width = dom.offsetWidth;
+
+    const bar = dom.querySelector('.bar');
+    const barHitBox = dom.querySelector('.bar-hit-area');
+    const handle = dom.querySelector('.handle');
+    const handleHitBox = dom.querySelector('.handle-hit-area');
+
+    const barRenderer = css(bar);
+    const handleRenderer = css(handle);
+    const handleHitBoxRenderer = css(handleHitBox);
+
+    const mapWidthToVal = interpolate([0, this.width], range);
+    this.x = value(0, (v) => {
+      val.set(mapWidthToVal(v));
+      handleHitBoxRenderer.set('x', v);
+      handleRenderer.set('backgroundColor', getColor());
     });
 
-listen(document, 'mouseup touchend')
-    .start(() => {
-        spring({
-            from: ballXY.get(),
-            velocity: ballXY.getVelocity(),
-            to: { x: 0, y: 0 },
-            stiffness: 200,
-            // mass: 1,
-            // damping: 10
-        }).start(ballXY);
-    });
+    if (getGradient) {
+      hue.addListener(() => {
+        barRenderer.set('background', getGradient());
+        handleRenderer.set('backgroundColor', getColor());
+      });
+    }
+
+    if (initialValue) {
+      this.x.set(initialValue * this.width);
+    }
+
+    handleHitBox.addEventListener('mousedown', this.startTracking);
+    handleHitBox.addEventListener('touchstart', this.startTracking, { passive: false });
+    barHitBox.addEventListener('click', this.goto);
+  }
+
+  startTracking = (e) => {
+    const trackPointer = pointer(e).start();
+    trackOffset(trackPointer.x, {
+      from: this.x.get(),
+      transform: pipe(
+        conditional(
+          (v) => v > this.width,
+          nonlinearSpring(SPRING_STRENGTH, this.width)
+        ),
+        conditional(
+          (v) => v < 0,
+          nonlinearSpring(SPRING_STRENGTH, 0)
+        )
+      ),
+      onUpdate: this.x
+    }).start();
+
+    document.addEventListener('mouseup', this.stopTracking);
+    document.addEventListener('touchend', this.stopTracking);
+  };
+
+  isOutOfBounds = (x) => (x <= 0 || x >= this.width);
+
+  stopTracking = (e) => {
+    // If out of range
+    if (this.isOutOfBounds(this.x.get())) {
+      this.snapWithinRange();
+    // Or within range
+    } else {
+      this.glide();
+    }
+
+    document.removeEventListener('mouseup', this.stopTracking);
+    document.removeEventListener('touchend', this.stopTracking);
+  };
+
+  snapWithinRange = () => {
+    physics({
+      from: this.x.get(),
+      velocity: this.x.getVelocity(),
+      to: clamp(0, this.width)(this.x.get()),
+      spring: 800,
+      friction: 0.85,
+      onUpdate: this.x
+    }).start();
+  };
+
+  glide = () => {
+    physics({
+      from: this.x.get(),
+      velocity: this.x.getVelocity(),
+      friction: 0.5,
+      transform: (v) => {
+        if (this.isOutOfBounds(v)) this.snapWithinRange();
+        return v;
+      },
+      onUpdate: this.x
+    }).start();
+  };
+
+  goto = (e) => {
+    physics({
+      from: this.x.get(),
+      to: e.pageX - e.target.getBoundingClientRect().left,
+      friction: 0.97,
+      onUpdate: this.x,
+      spring: 700
+    }).start();
+  };
+}
+
+new Slider({
+  dom: document.querySelector('.slider.hue'),
+  val: hue,
+  getColor: () => `hsl(${hue.get()}, 100%, 50%)`,
+  range: [0, 360]
+});
+new Slider({
+  dom: document.querySelector('.slider.saturation'),
+  val: saturation,
+  initialValue: 1,
+  getColor: () => `hsl(${hue.get()}, ${saturation.get()}, 50%)`,
+  getGradient: () => `linear-gradient(to right, hsl(${hue.get()}, 0%, 50%), hsl(${hue.get()}, 100%, 50%))`,
+  range: [0, 100]
+});
+new Slider({
+  dom: document.querySelector('.slider.lightness'),
+  val: lightness,
+  initialValue: 0.5,
+  getColor: () => `hsl(${hue.get()}, 100%, ${lightness.get()})`,
+  getGradient: () => `linear-gradient(to right, hsl(${hue.get()}, 100%, 0%), hsl(${hue.get()}, 100%, 50%), hsl(${hue.get()}, 100%, 100%))`,
+  range: [0, 100]
+});
+
+const swatchRenderer = css(document.querySelector('.color'));
+const updateSwatch = () => swatchRenderer.set('background', `hsl(${hue.get()}, ${saturation.get()}, ${lightness.get()})`);
+hue.addListener(updateSwatch);
+saturation.addListener(updateSwatch);
+lightness.addListener(updateSwatch);
